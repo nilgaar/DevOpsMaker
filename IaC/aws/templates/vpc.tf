8eb74af66612d8603e11d65eef5cde05a3e9c168
Nil <nilgallego@pm.me>
2024-06-08 21:20:55 +0200

    chore: Update AWS VPC Terraform variables to use consistent naming convention

--- IaC/aws/templates/vpc.tf
+++ IaC/aws/templates/vpc.tf
@@ -1,9 +1,5 @@
-variable "AWS_ACCESS_KEY_ID" {
-
-}
-variable "AWS_SECRET_ACCESS_KEY" {
-}
-
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
 
 provider "aws"{
     region="eu-west-1"
