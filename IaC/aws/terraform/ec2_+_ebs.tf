20c1759bbd4182cf4bc5631f22cc49b34effaefc
Nil <nilgallego@pm.me>
2024-06-23 20:54:08 +0200

    chore: Update AWS VPC Terraform templates and workflows

--- IaC/aws/terraform/ec2_+_ebs.tf
+++ IaC/aws/terraform/ec2_+_ebs.tf
@@ -1,20 +1,30 @@
-data "aws_ssm_parameter" "ubuntu22" {
-    name = "/aws/service/ami-ids/ubuntu/images/hvm-instance/ubuntu22.04"
-}
 
 
 resource "aws_ebs_volume" "ebs_example" {
-    availability_zone = "eu-west-1a"
-    size = 1
+  availability_zone = "eu-west-1a"
+  size              = 1
 }
 
 resource "aws_instance" "ec2example" {
-    ami = "ami-0705384c0b33c194c"
-    instance_type = "t2.micro"
-    availability_zone = "eu-west-1a"
-    ebs_block_device {
-        device_name = "/dev/sdh"
-        volume_id = "${aws_ebs_volume.ebs_example.id}"
-    }
+  ami               = "ami-0705384c0b33c194c"
+  instance_type     = "t2.micro"
+  availability_zone = "eu-west-1a"
+  ebs_block_device {
+    device_name = "/dev/sdh"
+    volume_id   = aws_ebs_volume.ebs_example.id
+  }
+  # subnet and security group from:  ./vpn_network.tf
+  subnet_id              = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.sec_group.id]
+
+  user_data = <<EOF
+    #!/bin/bash
+    sudo apt-get update -y
+    sudo apt-get install nginx -y
+    sudo systemctl enable nginx
+    sudo systemctl start nginx
+    sudo rm /var/www/html/index.nginx-debian.html
+    echo "<h1>Welcome to Terraform</h1>" | sudo tee /var/www/html/index.html
+    EOF
 }
 
--- IaC/aws/terraform/vpc_network.tf
+++ IaC/aws/terraform/vpc_network.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block         = "10.0.0.0/16"
   enable_dns_support = true
 }
 
@@ -8,8 +8,42 @@ resource "aws_internet_gateway" "gateaway" {
 }
 
 resource "aws_subnet" "subnet" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "eu-west-1a"
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "eu-west-1a"
   map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  # route all traffic into the internet gateway
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateaway.id
+  }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_security_group" "sec_group" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
