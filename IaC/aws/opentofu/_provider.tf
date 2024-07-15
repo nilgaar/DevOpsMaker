8ff6bce87048b2383c99b3438d33fb4949811fbb
Nil <nilgallego@pm.me>
2024-07-15 18:58:38 +0200

    chore: Update AWS VPC Terraform templates to use variable for availability zone

--- IaC/aws/opentofu/_provider.tf
+++ IaC/aws/opentofu/_provider.tf
@@ -7,11 +7,16 @@ variable "AWS_SECRET_ACCESS_KEY" {
   sensitive = true
 }
 
-variable "region" {
+variable "availability_zone" {
   type    = string
   default = "eu-west-1a"
 }
 
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+}
+
 provider "aws" {
-  region = "eu-west-1"
+  region = var.region
 }
